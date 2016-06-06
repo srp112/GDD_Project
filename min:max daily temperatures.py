42664dcd2b4b8aa66c15570f0a851bf6363c2776
liangyeni <liangyeni123@gmail.com>
2016-06-06 15:27:59 -0230

    min/max daily temperatures

---
+++ min:max daily temperatures.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt, pylab
+
+
+def max_min_temp(y, z, city):
+    fig = plt.figure()
+    x = np.linspace(0, 365, 365)
+    plt.subplot(2,3,1)
+    plt.plot(x, y, 'g', label="$max$")
+    plt.plot(x, z, 'r', label="$min$")
+    plt.legend()
+
+    plt.title("min/max daily temperatures" + city, color='green', fontsize='20')
+    plt.xlabel("days", fontsize='15')
+    plt.legent(loc=1)
+    plt.ylabel("temperatures", fontsize='15')
+    plt.legent(loc=2)
+
+    plt.show()
+
+max_min_temp(y, z, city)
+
+
+
+
+
