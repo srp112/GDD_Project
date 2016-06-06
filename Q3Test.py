1b4079bc4e9c937c1624983f0469a710b427057e
liangyeni <liangyeni123@gmail.com>
2016-06-06 13:58:05 -0230

    Q3Test
    This is for GDD Calculation.

---
+++ Q3Test.py
@@ -0,0 +1,16 @@
+import numpy as np
+import math
+
+data = np.loadtxt(open("/Users/air/Desktop/temperatures.csv"),delimiter=",",skiprows=0)
+data_sum = data.sum(axis = 1)
+base = 10
+
+def gdd(DataSet, base):
+    avg = DataSet.sum(axis = 1) / 2
+    gdd = avg - base
+    gdd = gdd.clip(min=0)
+
+    return gdd
+
+DataSet = data
+gdd(DataSet, 10)
