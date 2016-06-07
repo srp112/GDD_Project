a20b8d790097cc2a16344cbbeb513d4bf97cf3db
ms0185 <ms0185@mun.ca>
2016-06-07 04:46:35 -0230

    MinMax plotting
    This covers core task2

---
+++ MinMax/MinMax.py
@@ -0,0 +1,53 @@
+import sys, argparse, csv
+import numpy as np
+import scipy.linalg as la
+import matplotlib.pyplot as plt
+
+
+def open_file(file_name, mode):
+    """Open a file."""
+    try:
+        with open(sys.argv[1], 'r') as the_file:
+          filename=the_file.read()
+          
+    except IOError as err:
+        print("Unable to open the file", file_name, "Ending program.\n", err)
+        input("\n\nPress the enter key to exit.")
+        sys.exit()
+    else:
+        return filename
+
+"""Cities.txt file as an argument"""     
+try:
+   my_file = open_file(sys.argv[1], 'r')
+except:
+   print("MinMax needs one argument", "\nEnding program...\n")
+   input("Press the enter key to exit.")
+   sys.exit()
+
+print(my_file)
+extension=".csv"
+for line in my_file.splitlines():
+    file_name=line+extension
+    print(file_name) 
+    try:
+     with open(file_name, "rU") as files:
+      val = list(csv.reader(files))[26:]
+      #print(val)
+      max_temp = list(zip(*val))[5]
+      min_temp = list(zip(*val))[7]
+      plt.plot(max_temp)
+      plt.twinx()
+      plt.draw()
+      plt.show()
+      print(max_temp,min_temp)
+     
+    except IOError as err:
+       print("error", err)
+    
+
+
+
+
+
+
