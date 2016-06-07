3c54c40fbfe6cce1324d84264ca7fb5e8d0f85bd
liangyeni <liangyeni123@gmail.com>
2016-06-07 16:04:24 -0230

    Update Q3Test.py

--- Q3Test.py
+++ Q3Test.py
@@ -1,18 +1,18 @@
 import numpy as np
 import math
+import csv
+import matplotlib as plt
 
-data = np.loadtxt(open("/Users/air/Desktop/temperatures.csv"),delimiter=",",skiprows=0)
-data_sum = data.sum(axis = 1)
-base = 10
 
-def gdd(DataSet, base):
-    avg = DataSet.sum(axis = 1) / 2
-    gdd = avg - base
-    gdd = gdd.clip(min=0)
-
-    return gdd
-
-DataSet = data
-gdd(DataSet, 10)
-
-np.savetxt('/Users/air/Desktop/GDD_Calculation.csv', gdd(DataSet, 10), delimiter = ',')
+with open("/Users/air/Desktop/montreal.csv") as files:
+    
+    #val = list(csv.reader(files))[1:3]
+    val = list(csv.DictReader(files))
+    
+    #Max_Temp = list(zip(*val))[3]
+    #Min_Temp = list(zip(*val))[4]
+    #print(type(Max_Temp))
+    #print(type(Min_Temp))
+    for row in val:
+        GDD = ((float(row['Min Temp']) + float(row['Max Temp']))/2)-10
+        print(row['Min Temp'], ' ' , row['Max Temp'], ' ', str(round(GDD, 2)))
