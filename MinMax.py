30e37f24ccfc9258ebf508c4a3aa93fa2ce43084
ms0185 <ms0185@mun.ca>
2016-06-08 06:09:21 -0230

    MinMax.py - year as an argument
    implemented another argument that takes year
     $python MinMax.py cities.txt 2016
    it will plot data for 2016. Error if provided year’s data not available
     $No data available for Year - 2018

--- MinMax.py
+++ MinMax.py
@@ -3,9 +3,10 @@ import numpy as np
 import scipy.linalg as la
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+from  more_itertools import unique_everseen
 
 
-def open_file(file_name, mode):
+def MinMax(file_name,year, mode):
     """Open a file."""
     try:
         with open(sys.argv[1], 'r') as the_file:
@@ -18,52 +19,62 @@ def open_file(file_name, mode):
     else:
         return filename
 
-"""Cities.txt file as an argument"""     
+"""Cities.txt Year as an argument"""     
 try:
-   my_file = open_file(sys.argv[1], 'r')
+   my_file = MinMax(sys.argv[1],sys.argv[2], 'r')
 except:
-   print("MinMax needs one argument", "\nEnding program...\n")
+   print("MinMax needs two arguments", "\nEnding program...\n")
    input("Press the enter key to exit.")
    sys.exit()
 
-print(my_file)
+
 extension=".csv"
 for line in my_file.splitlines():
-    file_name=line+extension
+  file_name=line+extension
   
-    print(file_name) 
-    try:
-     with open('data/'+file_name, "rU") as files:
-      val = list(csv.reader(files))[1:]
-      #print(val)
-      max_temp = list(zip(*val))[3]
-      max_temp = ['0' if x is '' else x for x in max_temp] 
-      #print(b)
-      min_temp = list(zip(*val))[4]
-      min_temp = ['0' if x is '' else x for x in min_temp] 
-      #x=np.linspace(1,365,365, endpoint=True)
-      fig = plt.figure()
-      plt.plot(max_temp, 'r', label="Max")
-      plt.plot(min_temp, 'g', label="Min")
-      plt.xlabel("Day")
-      plt.ylabel("Temp.")
-      plt.title("Min/Max Daily Temp "+line,fontsize=18,color='g')
-      plt.legend()
-      plt.pause(2)
-      plt.draw()
-      figs=None
-      pp = PdfPages("Min_Max_Daily_Temp.pdf")
-      if figs is None:
-         figs = [plt.figure(n) for n in plt.get_fignums()]
-      for fig in figs:
-        fig.savefig(pp, format='pdf')
-      pp.close()
+  #print(file_name) 
+  try:
+    with open('data/'+file_name, "rU") as files:
       
+      user_input=sys.argv[2]
+      check_year_in_file=[x[0] for x in csv.reader(files)]
       
-      #print(max_temp,min_temp)
+      if user_input in check_year_in_file:
+       with open('data/'+file_name, "rU") as files: 
+        max_min_temp = [x[3:5] for x in csv.reader(files) if str(x[0]) == sys.argv[2]] #extracting min/max temp for the year passed as an argument
+        #print(max_min_temp)
+        max_temp=list(zip(*max_min_temp))[0]
+        max_temp = ['0' if x is '' else x for x in max_temp] 
+        min_temp = list(zip(*max_min_temp))[1]
+        min_temp = ['0' if x is '' else x for x in min_temp] 
+        fig = plt.figure()
+        plt.plot(max_temp, 'r', label="Max")
+        plt.plot(min_temp, 'g', label="Min")
+        plt.xlim(0,400)
+        plt.xlabel("Days")
+        plt.ylabel("Temperature")
+        title='Min/Max Daily Temp for ' +line + ' Year - '+sys.argv[2]
+        plt.title(title,fontsize=14)
+        plt.legend()
+        plt.pause(2)
+        plt.draw()
+        figs=None
+        save_plot = PdfPages("Min_Max_Daily_Temp.pdf")
+        if figs is None:
+           figs = [plt.figure(n) for n in plt.get_fignums()]
+        for fig in figs:
+          fig.savefig(save_plot, format='pdf')
+        save_plot.close()
+      else:
+       print("No data available for Year - "+sys.argv[2])  
+       break
+      
+
+
+       
      
-    except IOError as err:
-       print("error", err)
+  except IOError as err:
+    print("error", err)
     
 
 
