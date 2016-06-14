078c42942c5e6796e235cb2865e80e794c5e726e
srp112 <silvana.rpereira25@gmail.com>
2016-06-14 17:28:06 -0230

    Update GDD_map_Canada.py

--- GDD_map_Canada.py
+++ GDD_map_Canada.py
@@ -2,55 +2,63 @@ from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
-
+import csv
 FolderName = 'data'
+
+Data = []
+
+#Read GDD data
+with open(sys.argv[1].split('.')[0] + 'Map.txt', 'r') as cf:
+	for line in cf.read().splitlines():
+		CityData = line.split(',')
+		filename = CityData[0] + ',' + CityData[1]
+		with open(FolderName + '/' + filename + '.csv', 'r') as f:
+			reader = csv.DictReader(f)
+			GDDSum = 0
+			for row in reader:
+				if row['Year'] == '2015':
+					if row['GDD'] != '':
+						GDDSum += float(row['GDD'])
+			#City Name, latitude, longitude, GDD sum
+			Data.append((CityData[0], float(CityData[2]), float(CityData[3]), GDDSum))
 
 def get_marker_color(GDD):
     # Returns red for small GDD, yellow for moderate
     #  GDD, and green for significant GDD.
-    if GDD < 3.0:
+    if GDD < 510.0:
         return ('ro')
-    elif GDD < 7.0:
+    elif GDD < 900.0:
         return ('yo')
     else:
         return ('go')
 
 #Plotting map of Canada
-map = Basemap(llcrnrlon=-145.9927, llcrnrlat=44.49, urcrnrlon=-35.4459, urcrnrlat=71.8125,
-              projection='lcc', resolution = 'h', area_thresh = 10000.0,
+MyMap = Basemap(llcrnrlon=-145.9927, llcrnrlat=44.49, urcrnrlon=-35.4459, urcrnrlat=71.8125,
+              projection='lcc', resolution = 'h', area_thresh = 15000.0,
  lat_1=-4., lat_0=30.83158, lon_0=-50.)
 
+MyMap.drawmapboundary(fill_color='aqua')
+MyMap.fillcontinents(color='white', lake_color='aqua', zorder = 0)
+MyMap.drawcoastlines(color = '0.15')
+MyMap.drawcountries()
 
-map.drawmapboundary(fill_color='aqua')
-map.fillcontinents(color='grey', lake_color='aqua', zorder = 0)
-map.drawcoastlines(color = '0.15')
-map.drawcountries()
+lats = [city[1] for city in Data]
+lons = [city[2] for city in Data]
+magnitudes = [city[3] for city in Data]
+labels = [city[0] for city in Data]
 
-# Location of St. John's, Vancouver, Regina, Saskatoon, Montreal, Toronto
-lons = [-52.707866, -123.151382, -104.635418, -106.646736, -73.563254, -79.400041]
-lats = [47.570861, 49.406457, 50.834696, 52.139572, 45.500137, 43.670495] 
-# convert to map projection coords.
-# Note that lon,lat can be scalars, lists or numpy arrays.
-x,y = map(lons,lats)
-# convert back to lat/lon
+min_marker_size = 2.5
+for lon, lat, mag, name in zip(lons, lats, magnitudes, labels):
+	x,y = MyMap(lon, lat)
+	msize = mag * min_marker_size
+	print(name, ' ', mag)
+	marker_string = get_marker_color(mag)
+	MyMap.plot(x, y, marker_string, markersize=10)
+	plt.text(x+100000,y+150000, name) #distance of labels: 10 Km East and 10 Km North
 
-marker_string = get_marker_color(GDD)
-map.plot(x, y, marker_string, markersize=10)  # plot a colorful dot there
-
-labels = ['St. John\'s', 'Vancouver', 'Regina', 'Saskatoon', 'Montreal', 'Toronto']
-for label, xpt, ypt in zip(labels, x, y):
-    plt.text(xpt+100000,ypt+100000, label) #distance of labels: 10 Km East and 10 Km North
-
+plt.text(5, 1000000, r'GDD > 900', color='g')
+plt.text(5, 500000, r'GDD > 510', color='y')
+plt.text(5, 100000, r'GDD < 510', color='r')
+plt.text(5, 1500000, r'no data', color='w')
+plt.title(r'Acumulated GDD for 2015', fontsize=20, color='b')
 plt.show()
-
-#Read GDD data
-with open(sys.argv[1], 'r') as cf:
-	for filename in cf.read().splitlines():
-		GDD = []
-		with open(FolderName + '/' + filename + '.csv', 'r') as f:
-			reader = csv.DictReader(f)
-			for row in reader:
-				if row['GDD'] != '':
-					GDD.append(row)
-		for value in GDD:
-			print(value['Year'], '/', value['Month'], '/', value['Day'], ' ', value['GDD']) 
